02b714788cbac81ae6bef8fa74bad836ecc7b45f
Magno Gouveia PC <owlflee@gmail.com>
2016-04-16 15:54:37 -0300

    Primeiro Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerController : MonoBehaviour
+
+{
+    public bool wizardsRules = true;
+    public float animSpeed = 1.5f;
+    public bool useCurves = true;
+
+    public float useCurvesHeight = 0.5f;
+
+    public float forwardSpeed = 7.0f;
+    public float backwardSpeed = 2.0f;
+    public float rotateSpeed = 2.0f;
+    private CapsuleCollider col;
+    private Rigidbody rb;
+    private Vector3 velocity;
+
+    private GameObject mage;
+    private GameObject monster;
+    private GameObject player;
+
+    void Start()
+    {
+        col = GetComponent<CapsuleCollider>();
+        rb = GetComponent<Rigidbody>();
+        player = this.gameObject;
+
+        monster = player.transform.Find("Mutante").gameObject;
+        mage    = player.transform.Find("Mago").gameObject;
+
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = 1f;
+        rb.useGravity = true;
+
+        velocity = new Vector3(0, 0, v);
+        velocity = transform.TransformDirection(velocity);
+        if (v > 0.1)
+        {
+            velocity *= forwardSpeed;
+        }
+        else if (v < -0.1)
+        {
+            velocity *= backwardSpeed;
+        }
+
+        transform.localPosition += velocity * Time.fixedDeltaTime;
+
+        transform.Rotate(0, h * rotateSpeed, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            wizardsRules = !wizardsRules;
+        }
+
+        if (wizardsRules)
+        {
+            monster.SetActive(false);
+            mage.SetActive(true);
+        }
+        else {
+            monster.SetActive(true);
+            mage.SetActive(false);
+        }
+
+    }
+}
---
+++ Assets/Scripts/ScenarioManager.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScenarioManager : MonoBehaviour {
+    public GameObject ground;
+    public Vector3 size;
+
+    public float minOffset;
+    public float maxOffset;
+
+    public float maxLarg;
+    public float minLarg;
+
+    public float maxCenario;
+
+    private BoxCollider groundCollider;
+    private int grounds;
+
+    private GameObject[] arvoresDireita;
+    private GameObject[] arvoresEsquerda;
+
+    private float distancia;
+    // Use this for initialization
+    void Start () {
+        groundCollider = ground.gameObject.GetComponent<BoxCollider>();
+        size = groundCollider.size;
+        arvoresDireita = GameObject.FindGameObjectsWithTag("RightTree");
+        arvoresEsquerda = GameObject.FindGameObjectsWithTag("LeftTree");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        grounds = GameObject.FindGameObjectsWithTag("Ground").Length;
+
+        if (grounds <= 17) {
+            createPlatform();
+            criaCenarioAmplo();
+        }
+
+        distancia = 0f;
+        
+    }
+
+    void createPlatform() {
+        transform.position += new Vector3(0, 0, size.x);
+        Instantiate(ground, this.transform.position, transform.rotation);
+
+        for (var i = 0; i < 8; i++)
+        {
+            int escolhida = Random.Range(0, arvoresDireita.Length - 1);
+            GameObject arvore = (GameObject)arvoresDireita[escolhida];
+
+            distancia += Random.Range(minOffset, maxOffset);
+
+            float largeDistance = Random.Range(minLarg, maxLarg);
+            Instantiate(arvore, new Vector3(2f + largeDistance, 1f, this.transform.position.z-size.x + distancia), Quaternion.identity);
+        }
+
+        for (var i = 0; i < 8; i++)
+        {
+            int escolhida = Random.Range(0, arvoresEsquerda.Length - 1);
+            GameObject arvore = (GameObject)arvoresEsquerda[escolhida];
+
+            distancia += Random.Range(minOffset, maxOffset);
+            float largeDistance = Random.Range(minLarg, maxLarg);
+
+            Instantiate(arvore, new Vector3(-2f - largeDistance, 1f, this.transform.position.z - size.x + distancia), Quaternion.identity);
+        }
+
+    }
+
+    void criaCenarioAmplo() {
+
+        for (var i = 0; i < 8; i++)
+        {
+            int escolhida = Random.Range(0, arvoresDireita.Length - 1);
+            GameObject arvore = (GameObject)arvoresDireita[escolhida];
+
+            distancia += Random.Range(minOffset, maxOffset);
+
+            float largeDistance = Random.Range(minLarg, maxCenario);
+            Instantiate(arvore, new Vector3(3f + largeDistance, 1f, this.transform.position.z - size.x + distancia), Quaternion.identity);
+        }
+
+        for (var i = 0; i < 8; i++)
+        {
+            int escolhida = Random.Range(0, arvoresEsquerda.Length - 1);
+            GameObject arvore = (GameObject)arvoresEsquerda[escolhida];
+
+            distancia += Random.Range(minOffset, maxOffset);
+            float largeDistance = Random.Range(minLarg, maxCenario);
+
+            Instantiate(arvore, new Vector3(-3f - largeDistance, 1f, this.transform.position.z - size.x + distancia), Quaternion.identity);
+        }
+
+    }
+}
